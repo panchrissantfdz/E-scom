07a2bdfae77a1352b78932c84265bf60fd49746e
Christian <cfernandezm1700@alumno.ipn.mx>
2025-01-04 20:24:43 -0600

    es la buena

--- scripts/capture.js
+++ scripts/capture.js
@@ -7,21 +7,24 @@ document.getElementById('captureForm').addEventListener('submit', function(event
     const cantidad = document.getElementById('cantidad').value;
     const foto = document.getElementById('foto').files[0];
 
-    const reader = new FileReader();
+   const reader = new FileReader();
     reader.onloadend = function() {
-        const fotoData = reader.result.split(',')[1]; // Get base64 part
+    const fotoData = reader.result.split(',')[1]; // Extraer la parte Base64
+    // Asegurarte de que la foto no sea null o undefined
+    if (!fotoData) {
+        console.error('Error: No se pudo leer la imagen.');
+        return;
+    }
+    const newItem = { nombre, descripcion, precio: Number(precio), cantidad: Number(cantidad), foto: fotoData };
 
-        const newItem = { nombre, descripcion, precio, cantidad, foto: fotoData };
-
-        fetch('https://t9-2021630245.azurewebsites.net/api/AltaArticulo', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newItem)
+    fetch('https://t9-2021630245.azurewebsites.net/api/AltaArticulo', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newItem)
         }).then(res => res.json()).then(data => {
             console.log(data);
-            
         }).catch(error => console.error('Error:', error));
     };
     reader.readAsDataURL(foto);
